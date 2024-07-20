8d62aebd3162b25a353ce4fd7c14b7f4b4117211
prabhat135 <jainprabhat807@gmail.com>
2024-07-21 00:32:47 +0530

    added line.png instead of border-line

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,13 +3,15 @@ import React from "react";
 
 const AboutUs = () => {
   return (
-    <div className="aboutus flex flex-col lg:flex-row justify-around lg:px-20 dark:bg-white">
+    <div className="aboutus min-h-screen flex flex-col lg:flex-row justify-around lg:pl-8 dark:bg-white">
       <div className="aboutus-content flex flex-col items-center justify-center  lg:w-1/2 p-4 ">
         <div className="aboutus-heading items-center justify-center text-center lg:text-left">
-          <h1 className="text-center lg:text-left text-4xl sm:text-5xl xl:text-6xl text-white font-medium border-b-2 xl:px-10 pb-8 lg:pb-16 mb-8 lg:mb-16 leading-tight">
+          <h1 className="text-center lg:text-left text-4xl sm:text-5xl xl:text-6xl text-white font-medium xl:px-10 pb-8 lg:pb-16 leading-tight">
             We build <span className="text-[#04ABE2]">solutions &<br /> smart
             </span> delivering<br /> top- <span className="text-[#04ABE2]">notch service</span>
           </h1>
+
+          <img src="./assets/line.png" alt="line" className="w-3/4 lg:w-full xl:px-10 pb-8 lg:pb-10 mx-auto" />
 
           <p className=" flex flex-wrap px-10 sm:px-20 md:px-40 lg:px-0 xl:px-10 text-center lg:text-left justify-center items-center text-lg lg:text-2xl text-[#999999]">
             iTRACTION Technologies provides high-end web and mobile app
