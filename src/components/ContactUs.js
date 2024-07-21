71fac1837b08c76245ccdbbedddd52869ccdd154
prabhat135 <jainprabhat807@gmail.com>
2024-07-21 10:23:28 +0530

    fixes image alignment

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -5,7 +5,7 @@ const ContactUs = () => {
   return (
     <div className="contactus flex flex-col-reverse lg:flex-row justify-around p-4 lg:px-20 lg:pb-20 gap-8 dark:bg-white ">
 
-      <div className="contactus-image flex items-center mb-20 sm:mb-0 w-full lg:w-[50%]">
+      <div className="contactus-image flex items-center justify-center lg:justify-normal mb-20 sm:mb-0 w-full lg:w-[50%]">
         <img
           src="./assets/contactus.png"
           alt=""
