dbc2a38ff14e90ca68b7c5f435fc6d2e13ff701c
prabhat135 <jainprabhat807@gmail.com>
2024-07-22 00:43:16 +0530

    fixes font size for various screens

--- src/components/Service.js
+++ src/components/Service.js
@@ -6,10 +6,10 @@ const Service = () => {
     <section className="flex flex-col space-y-8 items-center justify-center h-screen">
       <div className="service-container flex flex-col md:flex-row justify-between items-center h-screen p-5 md:p-0 bg-[#151515] text-white w-full max-w-7xl">
         <div className="text-content flex flex-col px-5 md:pl-14 mb-7 md:mb-0 w-full md:w-[50%] text-center md:text-left">
-          <h1 className="text-4xl font-bold">
+          <h1 className="text-4xl sm:text-5xl xl:text-6xl text-center md:text-left pb-8 font-medium">
             our <span className="text-[#04ABE2]">services</span>
           </h1>
-          <p className="mt-4 text-lg">
+          <p className="text-center md:text-left justify-center items-center text-lg lg:text-2xl text-[#999999]">
             we offer global services, driving growth with expertly designed
             apps. let us elevate your business with cost-effective solutions and
             unlock its full potential.
