e7dbe35e97bc711dc8d417df067bccd99e4b7695
prabhat135 <jainprabhat807@gmail.com>
2024-07-22 21:52:16 +0530

    changed heading

--- src/components/Service.js
+++ src/components/Service.js
@@ -7,7 +7,7 @@ const Service = () => {
       <div className="service-container flex flex-col md:flex-row justify-between items-center h-screen p-5 md:p-0 bg-[#151515] text-white w-full max-w-7xl">
         <div className="text-content flex flex-col px-5 md:pl-14 mb-7 md:mb-0 w-full md:w-[50%] text-center md:text-left">
           <h1 className="text-4xl sm:text-5xl xl:text-6xl text-center md:text-left pb-8 font-medium">
-            our <span className="text-[#04ABE2]">services</span>
+            our <span className="text-[#04ABE2]">domains</span>
           </h1>
           <p className="text-center md:text-left justify-center items-center text-lg lg:text-2xl text-[#999999]">
             we offer global services, driving growth with expertly designed
