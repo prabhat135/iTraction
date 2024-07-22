2a8760ab0675f704739ff73294d229feb66e24fb
prabhat135 <jainprabhat807@gmail.com>
2024-07-22 11:36:20 +0530

    fix reviews font and updated marquee images

--- src/components/logo-cloude.js
+++ src/components/logo-cloude.js
@@ -1,42 +1,10 @@
-
 const logos = [
-    {
-      name: 'Vercel',
-      url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715881430/vercel_wordmark_dark_mhv8u8.svg',
-    },
-    {
-      name: 'Nextjs',
-      url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715881475/nextjs_logo_dark_gfkf8m.svg',
-    },
-    {
-      name: 'Prime',
-      url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/t2awrrfzdvmg1chnzyfr.svg',
-    },
-    {
-      name: 'Trustpilot',
-      url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/tkfspxqmjflfllbuqxsi.svg',
-    },
-    {
-      name: 'Webflow',
-      url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276560/logos/nymiivu48d5lywhf9rpf.svg',
-    },
-  
-    {
-      name: 'Airbnb',
-      url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/pmblusboe7vkw8vxdknx.svg',
-    },
-    {
-      name: 'Tina',
-      url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276560/logos/afqhiygywyphuou6xtxc.svg',
-    },
-    {
-      name: 'Stackoverflow',
-      url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/ts1j4mkooxqmscgptafa.svg',
-    },
-    {
-      name: 'mistral',
-      url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/tyos2ayezryjskox3wzs.svg',
-    },
+    { name: 'Star', url: "/assets/star.png" },
+    { name: 'Google', url: '/assets/google.png' },
+    { name: 'Star', url: "/assets/star.png" },
+    { name: 'Goodfirms', url: '/assets/goodfirms.png' },
+    { name: 'Star', url: "/assets/star.png" },
+    { name: 'Glassdoor', url: "/assets/glassdoor.png" },
   ]
   
   const AnimatedLogoCloud = () => {
@@ -53,7 +21,7 @@ const logos = [
           >
             {Array(5)
               .fill(null)
-              .map((index) => (
+              .map((_, index) => (
                 <div
                   key={index}
                   className="flex shrink-0 animate-logo-cloud flex-row justify-around gap-6"
@@ -62,7 +30,7 @@ const logos = [
                     <img
                       key={key}
                       src={logo.url}
-                      className="h-10 w-28 px-2 text-white  dark:invert"
+                      className="px-2 h-1/2 text-white dark:invert"
                       alt={`${logo.name}`}
                     />
                   ))}
@@ -75,5 +43,5 @@ const logos = [
     )
   }
   
-  export default AnimatedLogoCloud
+  export default AnimatedLogoCloud;
   
