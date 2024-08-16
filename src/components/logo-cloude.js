b39c1e3aab8e15c679c5c3e06049ac125b3b2538
prabhat135 <jainprabhat807@gmail.com>
2024-08-16 17:35:07 +0530

    fix light theme marquee images

--- src/components/logo-cloude.js
+++ src/components/logo-cloude.js
@@ -1,15 +1,15 @@
 const logos = [
-    { name: 'Star', url: "/assets/star.png" },
-    { name: 'Google', url: '/assets/google.png' },
-    { name: 'Star', url: "/assets/star.png" },
-    { name: 'Goodfirms', url: '/assets/goodfirms.png' },
-    { name: 'Star', url: "/assets/star.png" },
-    { name: 'Glassdoor', url: "/assets/glassdoor.png" },
+    { name: 'Star', url: "/assets/star.png", url_dark:"/assets/star_darktheme.png" },
+    { name: 'Google', url: '/assets/google.png', url_dark:"/assets/google_darktheme.png" },
+    { name: 'Star', url: "/assets/star.png", url_dark:"/assets/star_darktheme.png" },
+    { name: 'Goodfirms', url: '/assets/goodfirms.png', url_dark:"/assets/goodfirms_darktheme.png" },
+    { name: 'Star', url: "/assets/star.png", url_dark:"/assets/star_darktheme.png" },
+    { name: 'Glassdoor', url: "/assets/glassdoor.png", url_dark:"/assets/glassdoor_darktheme.png" },
   ]
   
   const AnimatedLogoCloud = () => {
     return (
-     <section className="w-full flex items-center justify-center h-full z-20 dark:bg-black">
+     <section className="w-full flex items-center justify-center h-full z-20">
        <div className="w-full pt-10 min-h-[28vh]">
         <div className="mx-auto w-full px-4 md:px-8">
           <div
@@ -30,7 +30,15 @@ const logos = [
                     <img
                       key={key}
                       src={logo.url}
-                      className="px-2 h-1/2 text-black dark:white"
+                      className="px-2 h-1/2 dark:hidden"
+                      alt={`${logo.name}`}
+                    />
+                  ))}
+                  {logos.map((logo, key) => (
+                    <img
+                      key={key}
+                      src={logo.url_dark}
+                      className="px-2 h-1/2 hidden dark:block"
                       alt={`${logo.name}`}
                     />
                   ))}
